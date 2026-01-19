350652fbf53e347e3506ed324cc1d1f951bc70ef
erdemck <erdemcankaradag@gmail.com>
2026-01-19 23:59:15 +0300

    Initialize Spring Project

---
+++ src/main/java/com/eck/Smart/Agenda/SmartAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.eck.Smart.Agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAgendaApplication.class, args);
+	}
+
+}
